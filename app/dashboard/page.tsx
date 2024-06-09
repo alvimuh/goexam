5320c618b64acbb0c97000ebb7bf0f73267f133a
alvimuh <mohamadalvin@gmail.com>
2024-06-09 17:00:06 +0700

    feat(trpc): init

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,12 @@
 import { Header } from "@/components/layout/header";
 import { Page } from "@/components/layout/page";
 import { MENU_LIST } from "@/lib/menu";
+import { trpc } from "@/lib/trpc";
 
 export default async function DemoPage() {
+  const hello = trpc.hello.useQuery({ text: "client" });
+  console.log({ hello });
+
   const menu = MENU_LIST["dashboard"].children["dashboard"];
   return (
     <Page>
